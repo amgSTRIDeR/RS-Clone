8cba3bdba903aacaf1b5c63635455d93f9de967a
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-02-06 17:27:24 +0400

    feat: start page render

---
+++ src/layers/View/View.ts
@@ -0,0 +1,27 @@
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import Start from '../components/Start/Start';
+
+export default class View {
+  private container: HTMLElement;
+
+  private header: Header;
+
+  private start: Start;
+
+  private footer: Footer;
+
+  constructor(container: HTMLElement) {
+    this.container = container;
+    this.header = new Header(this.container, 'light', 'en', false);
+    this.start = new Start(this.container);
+    this.footer = new Footer(this.container);
+    this.render();
+  }
+
+  private render(): void {
+    this.header.render();
+    this.start.render();
+    this.footer.render();
+  }
+}
