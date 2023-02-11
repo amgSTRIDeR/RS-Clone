110efc5305d5ac1f68eeacb24dc2d7ebbcc690b8
llallonen <llallonen@gmail.com>
2023-02-12 01:48:15 +0400

    refactor: change View temporarily

--- src/layers/View/View.ts
+++ src/layers/View/View.ts
@@ -1,27 +1,32 @@
+import Board from '../components/Board/Board';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
-import Start from '../components/Start/Start';
+// import Start from '../components/Start/Start';
 
 export default class View {
   private container: HTMLElement;
 
   private header: Header;
 
-  private start: Start;
+  private board: Board;
+
+  // private start: Start;
 
   private footer: Footer;
 
   constructor(container: HTMLElement) {
     this.container = container;
     this.header = new Header(this.container, 'corporate', 'en', false);
-    this.start = new Start(this.container);
+    this.board = new Board(this.container);
+    // this.start = new Start(this.container);
     this.footer = new Footer(this.container);
     this.render();
   }
 
   private render(): void {
     this.header.render();
-    this.start.render();
+    this.board.render();
+    // this.start.render();
     this.footer.render();
   }
 }
