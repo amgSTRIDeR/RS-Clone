eb3533fd66ce10d68ec65747aad23b3382b868f2
llallonen <llallonen@gmail.com>
2023-02-14 02:00:59 +0400

    refactor: change button' classes

---
+++ src/layers/components/Button/Button.ts
@@ -0,0 +1,46 @@
+import createElement from '../../../utils/create-element';
+import { IButtonProps } from './Button.types';
+
+export default class Button {
+  value: string = '';
+
+  className: string[] = [''];
+
+  onClick: Function | null;
+
+  constructor({ value, className, onClick }: IButtonProps) {
+    this.value = value ?? '';
+    this.className = className ?? [''];
+    this.onClick = onClick ?? null;
+  }
+
+  render() {
+    const ButtonInstance = createElement(
+      'button',
+      [
+        'button',
+        'w-[30px]',
+        'h-[30px]',
+        'p-[3px]',
+        'rounded',
+        'text-primary',
+        'border-2',
+        'border-primary',
+        'hover:text-secondary-focus',
+        'hover:border-primary-focus',
+        'hover:bg-primary-focus',
+        'transition',
+        'ease-in-out',
+        'delay-50',
+      ],
+      '',
+      `${this.value}`,
+    );
+
+    ButtonInstance.addEventListener('click', () => this.onClick);
+
+    return ButtonInstance;
+  }
+
+  onClickHandle = () => this.onClick;
+}
---
+++ src/layers/components/Button/ButtonWithIcon.ts
@@ -0,0 +1,46 @@
+import createElement from '../../../utils/create-element';
+import Button from './Button';
+import { IButtonTextWithIconProps } from './Button.types';
+
+export default class ButtonWithIcon extends Button {
+  svg?: string;
+
+  constructor({
+    value,
+    className,
+    onClick,
+    svg,
+  }: IButtonTextWithIconProps) {
+    super({ value, className, onClick });
+    this.svg = svg;
+  }
+
+  render() {
+    const ButtonInstance = createElement(
+      'button',
+      [
+        'button',
+        'flex',
+        'items-center',
+        'gap-2',
+        'w-[100px]',
+        'h-[30px]',
+        'p-[3px]',
+        'rounded',
+        'border-primary',
+        'text-primary',
+        'border-2',
+        'hover:text-secondary-focus',
+        'hover:border-primary-focus',
+        'hover:bg-primary-focus',
+        'transition',
+        'ease-in-out',
+        'delay-50',
+        `${this.className[0]}`,
+      ],
+      '',
+      `${this.svg} <span>${this.value}</span>`,
+    );
+    return ButtonInstance;
+  }
+}
