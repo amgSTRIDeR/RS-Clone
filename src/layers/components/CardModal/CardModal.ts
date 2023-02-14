03c17a5f16f028064d5070308a3d55fbeceb9231
llallonen <llallonen@gmail.com>
2023-02-15 03:18:32 +0400

    feat: add card modal buttons for adding user, copying link, etc

--- src/layers/components/CardModal/CardModal.ts
+++ src/layers/components/CardModal/CardModal.ts
@@ -1,10 +1,12 @@
 import createlement from '../../../utils/create-element';
 import testCLick from '../../../utils/test-click';
 import BoardSVG from '../Board/Board-svg';
+import ButtonTextWithIcon from '../Button/ButtonTextWithIcon';
 import ButtonWithIcon from '../Button/ButtonWithIcon';
 import Card from '../Card/Card';
 import { ICardProps } from '../Card/Card.types';
 import Editor from '../Editor/Editor';
+import CardModalAside from './CardModalAside';
 
 export default class CardModal extends Card {
   constructor({
@@ -69,7 +71,12 @@ export default class CardModal extends Card {
 
     const cardModalBody = createlement('div', ['card-modal__body']);
 
-    const cardModalDescriptionContainer = createlement('div', ['board__desc-box', 'flex', 'gap-3', 'my-5']);
+    const cardModalDescriptionContainer = createlement('div', [
+      'board__desc-box',
+      'flex',
+      'gap-3',
+      'my-5',
+    ]);
     const descriptionText = createlement('div', ['board__desc-text'], '', 'This description text');
     const cardModalDescription = createlement('div', ['board__desc'], '', `${BoardSVG.Lines}`);
     cardModalDescriptionContainer.append(cardModalDescription, descriptionText);
@@ -100,7 +107,12 @@ export default class CardModal extends Card {
     });
     cardModalMemberContainer.append(cardModalMember, cardModalMemberButton.render());
 
-    const cardModalMarksContainer = createlement('div', ['card__marks', 'flex', 'items-center', 'gap-3']);
+    const cardModalMarksContainer = createlement('div', [
+      'card__marks',
+      'flex',
+      'items-center',
+      'gap-3',
+    ]);
     const cardModalMark = createlement(
       'div',
       [
@@ -128,12 +140,82 @@ export default class CardModal extends Card {
 
     cardModalDetails.append(cardModalMemberContainer, cardModalMarksContainer);
 
+    const cardMoveButton = new ButtonTextWithIcon({
+      value: 'Move the card',
+      className: ['min-w-[200px]'],
+      onClick: testCLick,
+      svg: BoardSVG.Arrow,
+    }).render();
+
+    const cardCopyButton = new ButtonTextWithIcon({
+      value: 'Copy the card',
+      className: ['min-w-[200px]'],
+      onClick: testCLick,
+      svg: BoardSVG.Copy,
+    }).render();
+
+    const cardMakeTemplateButton = new ButtonTextWithIcon({
+      value: 'Make a template',
+      className: ['min-w-[200px]'],
+      onClick: testCLick,
+      svg: BoardSVG.Template,
+    }).render();
+
+    const cardDeleteButton = new ButtonTextWithIcon({
+      value: 'Make a template',
+      className: ['min-w-[200px]'],
+      onClick: testCLick,
+      svg: BoardSVG.Delete,
+    }).render();
+
+    const cardModalButtonsCol1 = createlement('div', [
+      'card__Buttons1',
+      'flex',
+      'md:flex-wrap2',
+      'gap-5',
+      'mt-5',
+    ]);
+    const cardModalButtonsCol2 = createlement('div', [
+      'card__Buttons2',
+      'flex',
+      'md:flex-wrap',
+      'gap-5',
+    ]);
+    cardModalButtonsCol1.append(cardMoveButton, cardCopyButton);
+
+    cardModalButtonsCol2.append(cardMakeTemplateButton, cardDeleteButton);
+
+    const cardModalActions = createlement('div', [
+      'card__Buttons',
+      'flex',
+      'flex-wrap',
+      'gap-5',
+      'mt-5',
+    ]);
+    cardModalActions.append(cardModalButtonsCol1, cardModalButtonsCol2);
+
     cardModalBody.append(
       cardModalDetails,
       cardModalDescriptionContainer,
       descriptionEditor.render(),
+      cardModalActions,
     );
-    cardModal.append(cardModalHeader, cardModalBody);
+
+    const cardModalMain = createlement('div', ['cardModal__main', 'flex', 'flex-col', 'md:flex-row', 'gap-0', 'md:gap-7']);
+    const cardModalAside = new CardModalAside({
+      container: this.container,
+      description: this.description,
+      name: this.name,
+      table: this.table,
+      column: this.column,
+      comments: this.comments,
+      users: this.users,
+      creator: this.creator,
+    }).render();
+
+    cardModalMain.append(cardModalBody, cardModalAside);
+
+    cardModal.append(cardModalHeader, cardModalMain);
     overlay.append(cardModal);
 
     this.container.append(overlay);
